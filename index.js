9a73fe92697767e892ec9849482fe98b95bc9027
Chaiyod Malingam <chaiyod.cymg@gmail.com>
2022-06-13 03:30:06 +0700

    commit

--- index.js
+++ index.js
@@ -158,66 +158,80 @@ app.post("/checkin",(req,res)=>{
         if (err) throw err;
      
         var dbo = db.db("mydb");
-        if(req.headers['x-api-key'] != undefined && req.headers['x-api-key'].toString() == "813c33ee-d292-4060-884e-ec2897401990asd99sd"){
-            const date_now = new Date(Date.now())
-            const date = date_now.toLocaleDateString('th-TH', {year: 'numeric',month: "short",day: 'numeric'})
-            const time = date_now.toLocaleTimeString('th-TH',{hour:"numeric",minute:"numeric"})
-          
-            var myobj1 = {date:date}
-       await   dbo.collection("time_checkin").find(myobj1).toArray(async function(err, result) {
-                if (err) throw err;
+        if(req.headers['x-api-key'] != undefined ){
 
-              let status = false
-                let json = {users:[{id:req.body.id, name:req.body.name, time:time}]}
+            var myobj1 = {_id:req.headers['x-api-key']}
+            await   dbo.collection("users").find(myobj1).toArray(async function(err, result) {
                 if(result.length > 0){
-           
-                       for (let value in result[0].users ) {
-                        console.log(result[0].users[value])
-                            if(result[0].users[value].id == req.body.id){
-                               
-                                status = false
-                                break
-                            }else{
-                                status = true
-                                json.users.push(result[0].users[value])
-                            
-                                
-                            }
-                    
-                        }
-                        if(status){
-                          
-                            var myquery = {date:date}
-                            var newvalues = { $set: json  };
-                        await  dbo.collection("time_checkin").updateOne(myquery, newvalues, function(err, result) {
-                            if (err) throw err;
-                            res.send({status:true,text:"เช็คอินเข้าทำงานสำเร็จ!"})
-                            db.close(); 
-                            });
-
-                        
-                        }else{
-                            res.send({status:false,text:"วันนี้คุณได้เช็คอินเข้าทำงานแล้ว ไม่สามารถเช็คอินซํ้าได้"})
-                        }
-                }
-                  else{
-                    
-                    
-                    var myobj2 = { _id:uuidv4(), date:date , users:[{id:req.body.id, name:req.body.name , time:time}] };
-                await    dbo.collection("time_checkin").insertOne(myobj2, function(err, result2) {
+                    const date_now = new Date(Date.now())
+                    const date = date_now.toLocaleDateString('th-TH', {year: 'numeric',month: "short",day: 'numeric'})
+                    const time = date_now.toLocaleTimeString('th-TH',{hour:"numeric",minute:"numeric"})
+                  
+                    var myobj1 = {date:date}
+               await   dbo.collection("time_checkin").find(myobj1).toArray(async function(err, result) {
                         if (err) throw err;
-                        console.log(result2)
-                        res.send({status:true,text:"เช็คอินเข้าทำงานสำเร็จ!"})
-                        db.close();
+        
+                      let status = false
+                        let json = {users:[{id:req.body.id, name:req.body.name, time:time}]}
+                        if(result.length > 0){
+                   
+                               for (let value in result[0].users ) {
+                                console.log(result[0].users[value])
+                                    if(result[0].users[value].id == req.body.id){
+                                       
+                                        status = false
+                                        break
+                                    }else{
+                                        status = true
+                                        json.users.push(result[0].users[value])
+                                    
+                                        
+                                    }
+                            
+                                }
+                                if(status){
+                                  
+                                    var myquery = {date:date}
+                                    var newvalues = { $set: json  };
+                                await  dbo.collection("time_checkin").updateOne(myquery, newvalues, function(err, result) {
+                                    if (err) throw err;
+                                    res.send({status:true,text:"เช็คอินเข้าทำงานสำเร็จ!"})
+                                    db.close(); 
+                                    });
+        
+                                
+                                }else{
+                                    res.send({status:false,text:"วันนี้คุณได้เช็คอินเข้าทำงานแล้ว ไม่สามารถเช็คอินซํ้าได้"})
+                                }
+                        }
+                          else{
+                            
+                            
+                            var myobj2 = { _id:uuidv4(), date:date , users:[{id:req.body.id, name:req.body.name , time:time}] };
+                        await    dbo.collection("time_checkin").insertOne(myobj2, function(err, result2) {
+                                if (err) throw err;
+                                console.log(result2)
+                                res.send({status:true,text:"เช็คอินเข้าทำงานสำเร็จ!"})
+                                db.close();
+                            })
+                        }
+                     
+                        
                     })
+                   
+                }else{
+                    res.status(403)
+                    res.send({message:"Forbidden"})
                 }
-             
-                
             })
-           
+
+          
 
 
           
+        }else{
+            res.status(403)
+            res.send({message:"Forbidden"})
         }
         
     })
